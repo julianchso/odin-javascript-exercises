f3781736a6f8a7e4bbf885fb3ad85321ad3d9c17
Julian So <ch.julian.so@gmail.com>
2022-12-02 09:26:34 -0500

    finish exercises 7 and 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,50 @@
-const add = function() {
-	
+const add = function (firstNum, secondNum) {
+  return firstNum + secondNum;
 };
 
-const subtract = function() {
-	
+const subtract = function (firstNum, secondNum) {
+  return firstNum - secondNum;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let total = 0;
+
+  array.forEach((num) => {
+    total += num;
+  });
+  return total;
 };
 
-const multiply = function() {
+const multiply = function (array) {
+  let total = 1;
 
+  array.forEach((num) => {
+    total *= num;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function (firstNum, secondNum) {
+  return firstNum ** secondNum;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num == 0) {
+    return 1;
+  }
+  let total = 1;
+
+  for (let i = num; i > 0; i--) {
+    console.log(i);
+    total *= i;
+  }
+  console.log(total);
+  return total;
 };
+
+factorial(2);
+factorial(5);
+factorial(10);
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +53,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
